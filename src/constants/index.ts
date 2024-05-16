af3576b0e366a8dad972717dfb12f534c2b9b9c6
The Dakshi <kdakshi2020@gmail.com>
2024-05-16 17:00:43 +0530

    deployment requirements api added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,11 +8,19 @@ const ROOT_BRANCH = `v/${CURRENT_VERSION_NAME}`;
 
 const RECURRING_JOB = "0 0 * * SUN";
 
+const DEPLOYMENT_REQUIREMENTS = [
+  "Update metadata app name",
+  "Upload metadata app logo",
+  "Update Onesignal app id",
+  "Enable In-app purchase from the app settings",
+];
+
 export {
   CURRENT_VERSION_NAME,
   CURRENT_VERSION_NUMBER,
+  customhostDeploymentDir,
+  DEPLOYMENT_REQUIREMENTS,
+  githubrepo,
   RECURRING_JOB,
   ROOT_BRANCH,
-  customhostDeploymentDir,
-  githubrepo,
 };
