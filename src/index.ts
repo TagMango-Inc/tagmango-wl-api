fdc3c2a874c193d34d92c7800ce47c02a6a0249d
Rohan Mehta <rhnmht30@gmail.com>
2024-12-30 13:09:48 +0530

    feat: add CORS support with allowed methods and headers in the main application

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,38 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import { Hono } from 'hono';
-import { cors } from 'hono/cors';
-import { logger } from 'hono/logger';
-import { prettyJSON } from 'hono/pretty-json';
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
 
-import { serve } from '@hono/node-server';
-import { serveStatic } from '@hono/node-server/serve-static';
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 
-import Mongo from './database';
-import authenticationMiddleware from './middleware/authentication';
-import appFormsRouter from './routers/appFormsRouter';
-import authenticationRouter from './routers/authenticationRouter';
-import customHostRouter from './routers/customHostRouter';
-import developerAccountsRouter from './routers/developerAccountsRouter';
-import iapRouter from './routers/iapRouter';
-import metadataRouter from './routers/metadataRouter';
-import outputRouter from './routers/outputRouter';
-import releaseRouter from './routers/releaseRouter';
-import sseRouter from './routers/sse';
-import userManagementRouter from './routers/userManagementRouter';
+import Mongo from "./database";
+import authenticationMiddleware from "./middleware/authentication";
+import appFormsRouter from "./routers/appFormsRouter";
+import authenticationRouter from "./routers/authenticationRouter";
+import customHostRouter from "./routers/customHostRouter";
+import developerAccountsRouter from "./routers/developerAccountsRouter";
+import iapRouter from "./routers/iapRouter";
+import metadataRouter from "./routers/metadataRouter";
+import outputRouter from "./routers/outputRouter";
+import releaseRouter from "./routers/releaseRouter";
+import sseRouter from "./routers/sse";
+import userManagementRouter from "./routers/userManagementRouter";
 
 const app = new Hono().basePath("/wl");
+
+app.options("*", (c) => {
+  c.header("Access-Control-Allow-Origin", "*");
+  c.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+  );
+  c.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  return c.text("", 204);
+});
 
 Mongo.connect().then(() => {
   app.use(logger());
